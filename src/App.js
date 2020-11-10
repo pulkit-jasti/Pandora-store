e3c6978efc806728f0e8a0646d406d6f70fe64b8
Pulkit Jasti <pulkit.jasti@gmail.com>
2020-11-10 17:03:11 +0530

    images updated

--- src/App.js
+++ src/App.js
@@ -5,72 +5,29 @@ import ProductCard from './components/ProductCard';
 
 import rand from './assets/test.jpeg';
 
+import data from './data';
+
+let request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+	console.log(request.responseText);
+});
+
 function App() {
 	return (
 		<main>
 			<section className='main-container'>
 				<h1 className='title'>Pandora's Store.</h1>
 				<div className='card-container'>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
-					<ProductCard
-						title='pultit'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
-						imgsrc={rand}
-					/>
+					{data.map(el => {
+						return (
+							<ProductCard
+								imgsrc={rand}
+								title={el.productTitle}
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic incidunt placeat provident, animi ipsam sit illo ad fugiat nemo impedit libero, nostrum eveniet numquam illum sequi quidem id. Ad.'
+							/>
+						);
+					})}
 				</div>
 			</section>
 		</main>
