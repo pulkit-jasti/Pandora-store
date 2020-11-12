64e5bc500dd1f2e2e874c9a4b8492061f578296d
Pulkit Jasti <pulkit.jasti@gmail.com>
2020-11-12 21:37:07 +0530

    Updated json data and order of cards, and removed extra images

--- src/data.js
+++ src/data.js
@@ -1,5 +1,26 @@
 let data = [
 	{
+		productTitle: "Philosopher's Stone",
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/philosphers-stone.png',
+		productCost: '100 Units',
+		productDes:
+			"Created by trapping and condensing human souls of ones who have passed with certain special transmutation circles, The Philosopher's Stone allows the user to ignore Alchemy's law of equivalent exchange",
+	},
+	{
+		productTitle: 'Khyber Saber',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/khyber-saber.png',
+		productCost: '100 Units',
+		productDes:
+			"Made by the legendary Jedi Master Kantoo, the khyber saber was the first and the most powerful light saber ever created, it was said to magnify the wielder's Force powers a thousandfold, granting them the ability to cut entire planets in half with a single swing",
+	},
+	{
+		productTitle: 'Delorean Time Machine',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/delorean-time-machine.png',
+		productCost: '100 Units',
+		productDes:
+			"The DeLorean time machine was Dr. Emmett Brown's most successful invention, a plutonium-powered time machine comprised of a DeLorean DMC-12 sports car that had to reach 88 miles per hour in order to time travel.",
+	},
+	{
 		productTitle: 'Gate of Truth',
 		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/gate-of-truth.png',
 		productCost: '100 Units',
@@ -7,11 +28,10 @@ let data = [
 			'Pure knowledge about the world, the universe, alchemy and life itself will be forced into your mind as you begin to break down both physically and mentally, becoming part of the flow.',
 	},
 	{
-		productTitle: 'Evangelion',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/evangelion.png',
+		productTitle: 'Death Star',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/death-star.png',
 		productCost: '100 Units',
-		productDes:
-			'Evangelion is a cyborg being of near-godlike power, created from samples of the First Angel, Adam and has been imbued with human a soul',
+		productDes: 'A Death Star was a gargantuan space station armed with a planet-destroying superlaser.',
 	},
 	{
 		productTitle: 'TARDIS',
@@ -28,70 +48,17 @@ let data = [
 			'Crafted by the Dark Lord Sauron in the fire of Orodruin, during the Second Age, The One Ring is the most powerful artifact ever created in Middle-earth. It can grant the weilder the power to control over all the other Rings of Power',
 	},
 	{
-		productTitle: 'R2D2',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/r2d2.png',
+		productTitle: 'Nimbus 2000',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/nimbus.png',
+		productCost: '100 Units',
+		productDes: 'The Nimbus 2000 is a broom produced by the Nimbus Racing Broom Company as part of their successful line of racing brooms.',
+	},
+	{
+		productTitle: 'Portal Gun',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/portal-gun.png',
 		productCost: '100 Units',
 		productDes:
-			'A smart, spunky R2-series astromech droid manufactured by Industrial Automatondroid. They sport a distinctive domed head, and are programmed to perform a variety of tasks.',
-	},
-	{
-		productTitle: 'C-3PO',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/c3po.png',
-		productCost: '100 Units',
-		productDes:
-			'C-3PO is a 3PO-series protocol droid designed to interact with organics, programmed primarily for etiquette and protocol and is fluent in over six million forms of communication "We\'re doomed"',
-	},
-	{
-		productTitle: "Philosopher's Stone",
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/philosphers-stone.png',
-		productCost: '100 Units',
-		productDes:
-			"Created by trapping and condensing human souls of ones who have passed with certain special transmutation circles, The Philosopher's Stone allows the user to ignore the law of equivalent exchange",
-	},
-	{
-		productTitle: 'Sword of Gryffindor',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/sword-gryffindor.png',
-		productCost: '100 Units',
-		productDes:
-			"The enchanted sword of Gryffindor was made a thousand years ago by goblins, the magical world's most skilled metalworkers, and was owned by the famed wizard Godric Gryffindor",
-	},
-	{
-		productTitle: 'Khyber Saber',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/khyber-saber.png',
-		productCost: '100 Units',
-		productDes:
-			"Made by the legendary Jedi Master Kantoo, the khyber saber was the first and the most powerful light saber ever created, it was said to magnify the wielder's Force powers a thousandfold, granting them the ability to cut entire planets in half with a single swing",
-	},
-	{
-		productTitle: "Mace Windu's Saber",
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/mace-windu-light-saber.png',
-		productCost: '100 Units',
-		productDes: 'Ah... If only he listened to what Anakin was saying... ',
-	},
-	{
-		productTitle: 'Command Block',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/command-block.png',
-		productCost: '100 Units',
-		productDes:
-			'Obtain near god-like powers with the ability called as "World Edit". This indestructible block grants the user the ablity to change behavior of every atom every block in the world.',
-	},
-	{
-		productTitle: 'Dark Saber',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/dark-saber.png',
-		productCost: '100 Units',
-		productDes: 'The misterious and powerful light saber was said to be created by Tarre Vizsla, the first mandelorian to join the Jedi Order',
-	},
-	{
-		productTitle: 'Morty',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/morty.png',
-		productCost: '100 Units',
-		productDes: 'Your very own morty for all your intergalactic and interdimensional adventures',
-	},
-	{
-		productTitle: "Darth Revan's Mask",
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/darth-revan-mask.png',
-		productCost: '100 Units',
-		productDes: 'An ancient artifact that belonged to the powerful Dark lord of the Sith, Darth Revan',
+			'The Portal Gun is a gadget that allows the users to travel between different universes / dimensions / realities. The Gun was likely created by a Rick, although it is unknown which one;',
 	},
 	{
 		productTitle: 'Omnitrix',
@@ -105,7 +72,7 @@ let data = [
 		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/ultimatrix.png',
 		productCost: '100 Units',
 		productDes:
-			'The ultimatrix surpasses the orignal omnitrix by adding an evolutionary function granting the aliens new powers and new abilities that are more powerful, Ultimate Aliens.',
+			'The ultimatrix surpasses the orignal omnitrix by adding an evolutionary function granting the aliens new powers and new abilities that are more powerful, called as Ultimate Aliens.',
 	},
 	{
 		productTitle: 'Biomnitrix',
@@ -115,24 +82,96 @@ let data = [
 			'The Biomnitrix is two Omnitricies that are linked together, thus allowing the user to perform alien fusions by slamming the two gauntlets together. These fusions are not stored in the Biomnitrix, rather, they are created when being transformed into.',
 	},
 	{
+		productTitle: 'Negatrix',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/negatrix.png',
+		productCost: '100 Units',
+		productDes:
+			"The Omnimatrix, nicknamed the Negatrix, is Nega Ben's version of the second Omnitrix. It's functions are similar to the orignal Omnitrix with the only difference being this is Black",
+	},
+	{
+		productTitle: 'Nemetrix',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/nemetrix.png',
+		productCost: '100 Units',
+		productDes:
+			'The Nemetrix is a DNA altering device designed by Dr. Psychobos just like the Omnitrix. It has the ability to transform the user into non-sapient alien beings that are actually the natural predators of Omnitrix aliens.',
+	},
+	{
+		productTitle: 'Antitrix',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/antitrix.png',
+		productCost: '100 Units',
+		productDes:
+			'The Antitrix is a DNA altering device, yet another knock-off alternative to the Omnitrix, designed by Vilgax and built by Kevin Levin on Earth',
+	},
+	{
+		productTitle: 'C-3PO',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/c3po.png',
+		productCost: '100 Units',
+		productDes:
+			'C-3PO is a 3PO-series protocol droid designed to interact with organics, programmed primarily for etiquette and protocol and is fluent in over six million forms of communication "We\'re doomed"',
+	},
+	{
+		productTitle: 'Morty',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/morty.png',
+		productCost: '100 Units',
+		productDes: 'Your very own morty for all your intergalactic and interdimensional adventures',
+	},
+	{
+		productTitle: 'Sword of Gryffindor',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/sword-gryffindor.png',
+		productCost: '100 Units',
+		productDes:
+			"The enchanted sword of Gryffindor was made a thousand years ago by goblins, the magical world's most skilled metalworkers, and was owned by the famed wizard Godric Gryffindor",
+	},
+	{
+		productTitle: "Mace Windu's Saber",
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/mace-windu-light-saber.png',
+		productCost: '100 Units',
+		productDes: 'Ah... If only he had listened to what Anakin was saying... ',
+	},
+	{
+		productTitle: 'Magical Compass',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/magical-compass.png',
+		productCost: '100 Units',
+		productDes: 'This magical compass does not point north, it points to whatever the owner truely desires',
+	},
+	{
+		productTitle: 'Command Block',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/command-block.png',
+		productCost: '100 Units',
+		productDes:
+			'Obtain near god-like powers with the ability called as "World Edit". This indestructible block grants the user the ablity to change behavior of every atom every block in the world.',
+	},
+	{
+		productTitle: 'Dark Saber',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/dark-saber.png',
+		productCost: '100 Units',
+		productDes: 'The misterious and powerful light saber was said to be created by Tarre Vizsla, the first mandalorian to join the Jedi Order',
+	},
+	{
+		productTitle: "Green Lantern's Ring",
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/green-lantern-ring.png',
+		productCost: '100 Units',
+		productDes:
+			'A Green Lantern Ring, also known as a Power Ring, is a piece of jewelry that grants the wearer incredible and incomprehensible powers and abilities by harnessing willpower from the emotional spectrum.',
+	},
+	{
+		productTitle: "Darth Revan's Mask",
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/darth-revan-mask.png',
+		productCost: '100 Units',
+		productDes: 'An ancient artifact that belonged to the powerful Dark lord of the Sith, Darth Revan',
+	},
+	{
 		productTitle: "Good ol' Wooden Sword",
 		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/wooden-sword.png',
 		productCost: '100 Units',
 		productDes: "Sure it ain't as durable or strong as a diamond sword but it can still get the job done",
 	},
 	{
-		productTitle: 'Delorean Time Machine',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/delorean-time-machine.png',
+		productTitle: 'Higher Ground',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/high-ground.png',
 		productCost: '100 Units',
-		productDes:
-			"The DeLorean time machine was Dr. Emmett Brown's most successful invention, a plutonium-powered time machine comprised of a DeLorean DMC-12 sports car that had to reach 88 miles per hour in order to time travel.",
+		productDes: 'When you want to defeat the chosen one....',
 	},
-	/*{
-		productTitle: 'Hoverboard',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/hoverboard.png',
-		productCost: '100 Units',
-		productDes: 'Hoverboard',
-	},*/
 	{
 		productTitle: 'Death Note',
 		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/death-note.png',
@@ -140,12 +179,6 @@ let data = [
 		productDes:
 			"Orignally used by Shinigami, 'The Gods of death', to extend their lifespans. The Human whose name is written in this note shall die and thus granting the owner, the power od death",
 	},
-	/*{
-		productTitle: 'Diamond Armour Set',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/diamond-armour.png',
-		productCost: '100 Units',
-		productDes: 'lorem ipsum',
-	},*/
 	{
 		productTitle: 'Endor Dragon',
 		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/endor-dragon.png',
@@ -161,36 +194,11 @@ let data = [
 			'The one in possession of this Heart holds the leverage to command Davy Jones and the Flying Dutchman, hence obtaining the power to rule the Seven Seas',
 	},
 	{
-		productTitle: 'Magical Compass',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/magical-compass.png',
-		productCost: '100 Units',
-		productDes: 'This magical compass does not point north, it points to whatever the owner truely desires',
-	},
-	{
-		productTitle: "Green Lantern's Ring",
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/green-lantern-ring.png',
+		productTitle: 'R2D2',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/r2d2.png',
 		productCost: '100 Units',
 		productDes:
-			'A Green Lantern Ring, also known as a Power Ring, is a piece of jewelry that grants the wearer incredible and incomprehensible powers and abilities by harnessing willpower from the emotional spectrum.',
-	},
-	{
-		productTitle: 'Higher Ground',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/high-ground.png',
-		productCost: '100 Units',
-		productDes: 'When you want to defeat the chosen one....',
-	},
-	{
-		productTitle: 'Nimbus 2000',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/nimbus.png',
-		productCost: '100 Units',
-		productDes: 'The Nimbus 2000 is a broom produced by the Nimbus Racing Broom Company as part of their successful line of racing brooms.',
-	},
-	{
-		productTitle: 'Portal Gun',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/portal-gun.png',
-		productCost: '100 Units',
-		productDes:
-			'The Portal Gun is a gadget that allows the users to travel between different universes / dimensions / realities. The Gun was likely created by a Rick, although it is unknown which one;',
+			'A smart, spunky R2-series astromech droid manufactured by Industrial Automatondroid. They sport a distinctive domed head, and are programmed to perform a variety of tasks.',
 	},
 	{
 		productTitle: 'Pokeball',
@@ -213,12 +221,6 @@ let data = [
 			' Dragon balls are large, crystalline spheres (of varying sizes and colors) that feature five-pointed stars. Individually, they have no powers, but united they have the ability to call forth the Eternal Dragon who can grant any one wish',
 	},
 	{
-		productTitle: 'Death Star',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/death-star.png',
-		productCost: '100 Units',
-		productDes: 'A Death Star was a gargantuan space station armed with a planet-destroying superlaser.',
-	},
-	{
 		productTitle: 'Elder Wand',
 		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/elder-wand.png',
 		productCost: '100 Units',
@@ -229,21 +231,15 @@ let data = [
 		productTitle: 'Celestialsapien DNA',
 		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/celestialsapien.png',
 		productCost: '100 Units',
-		productDes: 'Celestialsepians are omnipotent beings who can make their very thoughts become reality. ',
+		productDes:
+			'Celestialsepians are omnipotent beings who can make their very thoughts become reality. All you need a DNA altering device like the omnitrix and boom',
 	},
 	{
-		productTitle: 'Nemetrix',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/nemetrix.png',
+		productTitle: 'Evangelion',
+		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/evangelion.png',
 		productCost: '100 Units',
 		productDes:
-			'The Nemetrix is a DNA altering device designed by Dr. Psychobos just like the Omnitrix. It has the ability to transform the user into non-sapient alien beings that are actually the natural predators of Omnitrix aliens.',
-	},
-	{
-		productTitle: 'Antitrix',
-		productImgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Pandora-store/master/src/assets/product-images/antitrix.png',
-		productCost: '100 Units',
-		productDes:
-			'The Antitrix is a DNA altering device, yet another knock-off alternative to the Omnitrix, designed by Vilgax and built by Kevin Levin on Earth',
+			'Evangelion is a cyborg being of near-godlike power, created from samples of the First Angel, Adam and has been imbued with human a soul',
 	},
 ];
 
